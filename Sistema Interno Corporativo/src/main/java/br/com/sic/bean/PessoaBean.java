b14658fc90946709b97e7f28fa22e29e58ace4fb
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-08-07 21:16:29 -0300

    (#15) Funcionalidade de cadastro de pessoas

---
+++ Sistema Interno Corporativo/src/main/java/br/com/sic/bean/PessoaBean.java
@@ -0,0 +1,102 @@
+package br.com.sic.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+
+import org.omnifaces.util.Messages;
+
+import br.com.sic.dao.PessoaDAO;
+import br.com.sic.domain.Pessoa;
+
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class PessoaBean implements Serializable {
+	private Pessoa pessoa;
+	private List<Pessoa> pessoas;
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	@PostConstruct
+	public void listar() {
+		try {
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoas = pessoaDAO.listar("nome");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar as pessoas");
+			erro.printStackTrace();
+		}
+	}
+
+	public void novo() {
+		try {
+			pessoa = new Pessoa();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar uma nova pessoa");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		try {
+			pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionada");
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar selecionar uma pessoa");
+		}
+	}
+
+	public void salvar() {
+		try {
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoaDAO.merge(pessoa);
+
+			pessoas = pessoaDAO.listar("nome");
+
+			pessoa = new Pessoa();
+
+			Messages.addGlobalInfo("Pessoa salva com sucesso");
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar salvar a pessoa");
+			erro.printStackTrace();
+		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			pessoa = (Pessoa) evento.getComponent().getAttributes().get("pessoaSelecionada");
+
+			PessoaDAO pessoaDAO = new PessoaDAO();
+			pessoaDAO.excluir(pessoa);
+
+			pessoas = pessoaDAO.listar();
+
+			Messages.addGlobalInfo("Pessoa removida com sucesso");
+		} catch (Exception erro) {
+			Messages.addFlashGlobalError("Ocorreu um erro ao tentar remover a pessoa");
+			erro.printStackTrace();
+		}
+	}
+
+}
---
+++ Sistema Interno Corporativo/src/main/java/br/com/sic/dao/PessoaDAO.java
@@ -0,0 +1,7 @@
+package br.com.sic.dao;
+
+import br.com.sic.domain.Pessoa;
+
+public class PessoaDAO extends GenericDAO<Pessoa> {
+
+}
