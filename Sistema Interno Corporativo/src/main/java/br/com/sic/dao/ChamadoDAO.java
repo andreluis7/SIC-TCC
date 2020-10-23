b21c50194495b0bfa7cb62c09b130c9bc2e7ffe1
Andre Luis <adrluis7@gmail.com>
2020-10-22 21:59:50 -0300

    (#40.1) Continuacao da codificacao da funcionalidade de chamados

--- Sistema Interno Corporativo/src/main/java/br/com/sic/dao/ChamadoDAO.java
+++ Sistema Interno Corporativo/src/main/java/br/com/sic/dao/ChamadoDAO.java
@@ -1,7 +1,35 @@
 package br.com.sic.dao;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
 import br.com.sic.domain.Chamado;
+import br.com.sic.util.HibernateUtil;
 
 public class ChamadoDAO extends GenericDAO<Chamado> {
+
+	public Chamado buscarChamado(Long codigoManutencao, Long codigoUsuario) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+
+		try {
+
+			Criteria consulta = sessao.createCriteria(Chamado.class);
+			consulta.createAlias("manutencao", "m");
+//			consulta.createAlias("usuario", "u");
+
+			consulta.add(Restrictions.eq("m.codigo", codigoManutencao));
+//			consulta.add(Restrictions.eq("u.codigo", codigoUsuario));
+
+			Chamado resultado = (Chamado) consulta.uniqueResult();
+
+			return resultado;
+
+		} catch (RuntimeException erro) {
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
 
 }
